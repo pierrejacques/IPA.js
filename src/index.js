ce790d90d587f69c1046507ce2f9992ab367dd7f
金辰浩 <jinchenhao@meituan.com>
2018-01-30 14:09:01 +0800

    combine recursoin core into index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-import core from './recursionCore.js';
+import stategies from './stategies/index.js'
+
+function getRecursion(method) {
+    const execute = (template, ...params) => {
+        let ret;
+        for (let i = 0; i < stategies.length; i++) {
+            const stategy = stategies[i];
+            if (stategy.condition(template)) {
+                ret = stategy[method](template, ...params, execute);
+                break;
+            }
+        }
+        return ret;
+    }
+    return execute;
+}
+
+const core = {
+    check: getRecursion('check'),
+    guarantee: getRecursion('guarantee'),
+    mock: getRecursion('mock'),
+    cache: {},
+    init(key, cache = {}) {
+        this.cache = cache;
+        this[key].asset = this;
+    },
+};
 
 export default class IPA {
     constructor(template) {
