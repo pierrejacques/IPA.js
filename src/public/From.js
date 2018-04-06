55941b1a7d3209f5346767a822d012ea57f20d9f
金辰浩 <jinchenhao@meituan.com>
2018-04-06 21:34:47 +0800

    new logic for From

--- src/public/From.js
+++ src/public/From.js
@@ -1,40 +1,24 @@
-import { cloneDeep, random, isObject } from 'lodash';
+import { isArray, cloneDeep, random, isEqual } from 'lodash';
 
-export default (template, isJSONcompare = false) => {
+export default (template, deep = false) => {
     const set = [];
     try {
         template.forEach((v) => {
-            if (isObject(v)) {
-                try {
-                    JSON.stringify(v);
-                } catch (e) {
-                    throw new Error('params of function "From" containes un-stringifiable object, most probably circular object');
-                }
-            }
             set.push(v);
         });
     } catch (e) {
-        throw new Error('function "From" only accepts iterable objects');
+        throw new Error('in IPA.From: the 1st param must be iterable');
     }
     const n = set.length;
     const getRandom = () => {
         const v = set[random(0, n - 1)];
-        return isJSONcompare ? cloneDeep(v) : v;
+        return deep ? cloneDeep(v) : v;
     };
     return () => ({
         check(val) {
             for (let i = 0; i < n; i++) { // eslint-disable-line
-                if (!isJSONcompare && set[i] === val) { // strict compare
+                if (!deep && set[i] === val || deep && isEqual(set[i], val)) {
                     return true;
-                }
-                if (isJSONcompare) {
-                    let result;
-                    try {
-                        result = JSON.stringify(set[i]) === JSON.stringify(val);
-                    } catch (e) {
-                        continue; // eslint-disable-line
-                    }
-                    if (result) return true;
                 }
             }
             return false;
@@ -42,6 +26,6 @@ export default (template, isJSONcompare = false) => {
         guarantee(val, strict) {
             return this.check(val) ? val : strict ? set[0] : getRandom();
         },
-        mock: prod => prod ? set[0] : getRandom,
+        mock: prod => !prod ? getRandom() : deep ? cloneDeep(set[0]) : set[0],
     });
 };
