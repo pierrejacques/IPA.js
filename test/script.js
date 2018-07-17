faa7fec3e44403214e7561b720660a4589bed98e
金辰浩 <jinchenhao@meituan.com>
2018-07-17 19:04:56 +0800

    fixing recurse catcher

--- test/script.js
+++ test/script.js
@@ -1,136 +1,149 @@
 const IPA = require('../dist/ipa.js');
-const { recurse } = IPA;
-// const basicSchema = new IPA({
-//     key: String,
-//     title: String,
-// });
+const { From, recurse } = IPA;
+const basicSchema = new IPA({
+    key: String,
+    title: String,
+});
 
-// const leafSchema = new IPA({
-//     valueType: From('number', 'date', 'string'),
-//     isLeaf: From(true),
-// });
+const leafSchema = new IPA({
+    valueType: From('number', 'date', 'string'),
+    isLeaf: From(true),
+});
 
-// const ipa = new IPA(({ catcher }) => {
-//     const guaranteeProvider = (val) => {
-//         if (!catcher.catch('an array', Array.isArray(val))) {
-//             return [];
-//         }
-//         val.forEach((item, idx) => {
-//             val[idx] = catcher.wrap(
-//                 idx,
-//                 () => {
-//                     const v = basicSchema.guarantee(item);
-//                     if (v.children) {
-//                         v.children = catcher.wrap(
-//                             'children',
-//                             () => guaranteeProvider(v.children),
-//                         );
-//                         return v;
-//                     }
-//                     return leafSchema.guarantee(v);
-//                 }
-//             );
-//         });
-//         return val;
-//     };
-//     return {
-//         guarantee: guaranteeProvider,
-//     };
-// }).onError((err) => {
-//     console.log(err);
-// });
+const ipa = new IPA(({ catcher }) => {
+    const guaranteeProvider = (val) => {
+        if (!catcher.catch('an array', Array.isArray(val))) {
+            return [];
+        }
+        val.forEach((item, idx) => {
+            val[idx] = catcher.wrap(
+                idx,
+                () => {
+                    const v = basicSchema.guarantee(item);
+                    if (v.children) {
+                        v.children = catcher.wrap(
+                            'children',
+                            () => guaranteeProvider(v.children),
+                        );
+                        return v;
+                    }
+                    return leafSchema.guarantee(v);
+                }
+            );
+        });
+        return val;
+    };
+    return {
+        guarantee: guaranteeProvider,
+    };
+}).onError((err) => {
+    console.log('1: ', err);
+});
 
-// const data =  [
-//     {
-//         key: '0',
-//         title: '层级1',
-//         children: [
-//             {
-//                 key: 1,
-//                 title: '二级1',
-//                 children: [
-//                     {
-//                         key: 0,
-//                         title: '城市a他娘的特别长，能起这么长名字的城市肯定都是傻逼，真的很长',
-//                         valueType: 'number',
-//                         isLeaf: null
-//                     },
-//                     {
-//                         key: '$1',
-//                         title: '城市b',
-//                         valueType: 'number',
-//                         isLeaf: true
-//                     },
-//                     {
-//                         key: '$2',
-//                         title: '城市c',
-//                         valueType: 'number',
-//                         isLeaf: true                       
-//                     },
-//                     {
-//                         key: '$3',
-//                         title: '城市d',
-//                         valueType: 'number',
-//                         isLeaf: true                       
-//                     },
-//                     {
-//                         key: '$4',
-//                         title: '城市e',
-//                         valueType: 'number',
-//                         isLeaf: true                       
-//                     },
-//                     {
-//                         key: '$5',
-//                         title: '城市f',
-//                         valueType: 'number',
-//                         isLeaf: null                       
-//                     }
-//                 ]
-//             },
-//             {
-//                 key: 2,
-//                 title: 2,
-//                 children: [
-//                     {
-//                         key: '$2',
-//                         title: '城市三',
-//                         valueType: 'number',
-//                         isLeaf: true
-//                     },
-//                     {
-//                         key: '$3',
-//                         title: '城市四',
-//                         valueType: 'number',
-//                         isLeaf: true
-//                     },
-//                     {
-//                         key: '$a',
-//                         title: '城市三a',
-//                         valueType: 'number',
-//                         isLeaf: true
-//                     },
-//                     {
-//                         key: '$b',
-//                         title: '城市四a',
-//                         valueType: 'number',
-//                         isLeaf: true
-//                     },
-//                     {
-//                         key: '$c',
-//                         title: '城市三b',
-//                         valueType: 'number',
-//                         isLeaf: true
-//                     },
-//                     {
-//                         key: '$d',
-//                         title: '城市四b',
-//                         valueType: 'number',
-//                         isLeaf: true
-//                     }
-//                 ]
-//             }
-//         ]
-//     }
-// ];
+const ipa2 = new IPA(recurse([{
+    key: String,
+    title: String,
+    isLeaf: From(true, undefined),
+    valueType: From('number', 'date', 'string', undefined),
+    children: '$$',
+}], {
+    border: From(undefined),
+})).onError((err) => {
+    console.log('2: ', err);
+});
 
-// ipa.guarantee(data);
+const data = [
+    {
+        key: '0',
+        title: '层级1',
+        children: [
+            {
+                key: 1,
+                title: '二级1',
+                children: [
+                    {
+                        key: 0,
+                        title: '城市a他娘的特别长，能起这么长名字的城市肯定都是傻逼，真的很长',
+                        valueType: 'number',
+                        isLeaf: null
+                    },
+                    {
+                        key: '$1',
+                        title: '城市b',
+                        valueType: 'number',
+                        isLeaf: true
+                    },
+                    {
+                        key: '$2',
+                        title: '城市c',
+                        valueType: 'number',
+                        isLeaf: true                       
+                    },
+                    {
+                        key: '$3',
+                        title: '城市d',
+                        valueType: 'number',
+                        isLeaf: true                       
+                    },
+                    {
+                        key: '$4',
+                        title: '城市e',
+                        valueType: 'number',
+                        isLeaf: true                       
+                    },
+                    {
+                        key: '$5',
+                        title: '城市f',
+                        valueType: 'number',
+                        isLeaf: null                       
+                    }
+                ]
+            },
+            {
+                key: 2,
+                title: 2,
+                children: [
+                    {
+                        key: '$2',
+                        title: '城市三',
+                        valueType: 'number',
+                        isLeaf: true
+                    },
+                    {
+                        key: '$3',
+                        title: '城市四',
+                        valueType: 'number',
+                        isLeaf: true
+                    },
+                    {
+                        key: '$a',
+                        title: '城市三a',
+                        valueType: 'number',
+                        isLeaf: true
+                    },
+                    {
+                        key: '$b',
+                        title: '城市四a',
+                        valueType: 'number',
+                        isLeaf: true
+                    },
+                    {
+                        key: '$c',
+                        title: '城市三b',
+                        valueType: 'number',
+                        isLeaf: true
+                    },
+                    {
+                        key: '$d',
+                        title: '城市四b',
+                        valueType: 'number',
+                        isLeaf: true
+                    }
+                ]
+            }
+        ]
+    }
+];
+
+ipa.guarantee(data);
+ipa2.guarantee(data);
